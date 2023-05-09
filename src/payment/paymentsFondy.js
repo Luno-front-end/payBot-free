0c040d7c454d77aea25adde15da0b4743bc7fae0
Luno-front-end <Onlyrobochaposhta@gmail.com>
2023-05-09 22:51:56 +0300

    add new request

--- src/payment/paymentsFondy.js
+++ src/payment/paymentsFondy.js
@@ -1,15 +1,24 @@
 const axios = require("axios");
+const https = require("https");
 const { resData, requestData, recurringData } = require("../payment/dataReq");
 
 require("dotenv").config();
 
-const postUrl = "https://pay.fondy.eu/api/checkout/url/";
-const resUrl = "https://pay.fondy.eu/api/status/order_id";
-const recurringUrl = "https://pay.fondy.eu/api/recurring";
+// const postUrl = "https://pay.fondy.eu/api/checkout/url/";
+// const resUrl = "https://pay.fondy.eu/api/status/order_id";
+// const recurringUrl = "https://pay.fondy.eu/api/recurring";
+
+const baseURL = "https://pay.fondy.eu/api";
+
+const axiosInstance = axios.create({
+  timeout: 60000, //optional
+  httpsAgent: new https.Agent({ keepAlive: true }),
+  maxRedirects: 5, // Задати максимальну кількість перенаправлень
+});
 
 const reqFondy = async () => {
-  return await axios
-    .post(postUrl, requestData)
+  return await axiosInstance
+    .post(`${baseURL}/checkout/url/`, requestData)
     .then((res) => res.data)
     .catch((error) => {
       console.log(error);
@@ -17,8 +26,8 @@ const reqFondy = async () => {
 };
 
 const resPayment = async () => {
-  return await axios
-    .post(resUrl, resData)
+  return await axiosInstance
+    .post(`${baseURL}/status/order_id`, resData)
     .then((res) => res.data)
     .catch((error) => {
       console.log(error);
@@ -26,8 +35,8 @@ const resPayment = async () => {
 };
 
 const recurringPay = async () => {
-  return await axios
-    .post(recurringUrl, recurringData)
+  return await axiosInstance
+    .post(`${baseURL}/api/recurring`, recurringData)
     .then((res) => res.data)
     .catch((error) => {
       console.log(error);
